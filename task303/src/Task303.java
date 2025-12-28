b02f879eeb5fd08e96c0087f20dcb1084cd525f7
nve123 <nv2011e@mail.ru>
2025-12-28 23:09:04 +0300

    complete

---
+++ task303/src/Task303.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+//Задача №303. Коровы
+//Ввод и вывод данных производятся через стандартные потоки ввода-вывода.
+//По данному числу n закончите фразу "На лугу пасется..." одним из возможных продолжений: "n коров", "n корова", "n коровы", правильно склоняя слово "корова".
+//
+//Входные данные
+//Дано число n (n<100).
+//
+//Выходные данные
+//Программа должна вывести введенное число n и одно из слов (на латинице): korov, korova или korovy, например, 1 korova, 2 korovy, 5 korov. Между числом и словом должен стоять ровно один пробел.
+//
+//Примеры
+//Входные данные
+//1
+//Выходные данные
+//1 korova
+
+public class Task303 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int korova = in.nextInt();
+        String sklonSlovaKorov;
+        if (korova % 100 >= 11 && korova % 100 <= 19) {
+            sklonSlovaKorov = "korov";
+        } else {
+            int lastNomber = korova % 10;
+            if (lastNomber == 1) {
+                sklonSlovaKorov = "korova";
+            } else if (lastNomber >= 2 && lastNomber <= 4) {
+                sklonSlovaKorov = "korovy";
+            } else {
+                sklonSlovaKorov = "korov";
+            }
+        }
+        System.out.println(korova + " " + sklonSlovaKorov);
+    }
+}
